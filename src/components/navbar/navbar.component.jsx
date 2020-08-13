a5dc93c3f01b2f1dbac1875cf04f35d53b343d7d
diegojuan2104 <diegojuan2104@gmail.com>
2020-08-12 21:19:08 -0500

    navbar advance 2

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import  './nabvar.styles.css';
+import './nabvar.styles.css';
 import './navbar.utils'
+import { ReactComponent as CartLogo } from "../../Assets/shopping-bag.svg";
 
 
 const Header = () => (
     <nav className="navbar">
         <ul className="menu">
-            <li className="logo"><a href="#">Logo</a></li>
-            <li className="item"><a href="#">Shop</a></li>
-            <li className="item"><a href="#">Contact</a></li>
-            <li className="item button secondary"><a href="#">Cart</a></li>
-            <li className="item button"><a href="#">SignIn</a></li>
+            <div className=""></div>
+            <li className="logo"><a href="#">LOGO</a></li>
+
+            <li className="item"><a href="#">SHOP</a></li>
+            <li className="item"><a href="#">CONTACT</a></li>
+            <li className="logo "><input class=" form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/></li>
+
+            <li className="button secondary "><a className="logo-container" href="#"><CartLogo /></a></li>
             <li id="toggle" className="toggle"><span class="bars"></span></li>
         </ul>
     </nav>
--- src/components/navbar/navbar.utils.js
+++ src/components/navbar/navbar.utils.js
@@ -4,8 +4,10 @@ $(function () {
     $(".toggle").on("click", function(){
         if($(".item").hasClass("active")){
             $(".item").removeClass("active")
+            $(".deploy").removeClass("active")
         }else{
             $(".item").addClass("active")
+            $(".deploy").addClass("active")
         }
     })
     console.log("ready!");
